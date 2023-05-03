0fa7302e83d1581b0088b8fd9b4c0ab2eb454cbd
Mina Gerges <100945436+mina-girges@users.noreply.github.com>
2023-05-03 21:02:52 +0300

    Add files via upload

---
+++ Update.r
@@ -0,0 +1,15 @@
+Data : ( 2023/04/27 )
+=============================
+Media_File   = "media_pic.css" "pic.html" "media.html" "media_pic_but_card.css"                     cheak  (✓)  (uploded)
+Media_Folder = "modes/pic"                                                                          cheak  (✓)  (uploded)
+
+Data : ( 2023/04/29 )
+=============================
+Media_File   = "media_pic.css" "pic.html" "pic_2.html" "media.html" "media_pic_but_card.css"        cheak  (✓)  (uploded)
+Media_Folder = "media/pic_3.html" "modes/pic"                                                       cheak  (✓)  (uploded)  
+
+Data : ( 2023/04/30 )
+=============================
+Agpeya_File  = "saa-12" 
+Index = "index.html"
+
